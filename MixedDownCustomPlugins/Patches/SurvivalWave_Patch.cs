d40397d51e39d69adb99ee9a4bea2c37f5c927e1
tru0067 <33011616+tru0067@users.noreply.github.com>
2025-02-07 15:37:27 +1100

    Add patch to fix spawn direction randomization

---
+++ MixedDownCustomPlugins/Patches/SurvivalWave_Patch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace MixedDownCustomPlugins.Patches
+{
+    [HarmonyPatch]
+    internal class SurvivalWave_Patch
+    {
+        [HarmonyPatch(typeof(SurvivalWave), nameof(SurvivalWave.GetScoredSpawnPoint))]
+        [HarmonyPrefix]
+        public static bool SurvivalWave_GetScoredSpawnPoint_Patch(
+            SurvivalWave __instance,
+            ref SurvivalWave.ScoredSpawnPoint __result
+        )
+        {
+            // Generate a vector which is actually uniform.
+            float angle = UnityEngine.Random.Range(0.0f, 2 * Mathf.PI);
+            Vector3 vector3 = new(Mathf.Cos(angle), 0.0f, Mathf.Sin(angle));
+            __result = __instance.GetScoredSpawnPoint(vector3.normalized);
+            return false;
+        }
+    }
+}
